3700d5dce5a89851150c0fde831790662451b23a
Samra Mammadli <memmedlisemra6@gmail.com>
2024-02-03 13:23:49 +0400

    Add authentification

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using SMlibraryApp.Dtos;
@@ -10,12 +8,27 @@ namespace SMlibraryApp.Controllers;
 public class IdentityController :Controller
 {
     [HttpGet]
-    public async Task<IActionResult> Login(){
+    public async Task<IActionResult> Login(string? returnUrl){
+        base.ViewData["returnUrl"] = returnUrl;
         return View();
     }
     [HttpPost]
-    public async Task<IActionResult> Login([FromForm] LoginDto login){
-        var schemes = CookieAuthenticationDefaults.AuthenticationScheme;
-        return Ok();
+    public async Task<IActionResult> Login([FromForm] LoginDto loginDto){
+        var claims = new Claim[] {
+            new Claim(ClaimTypes.Name, loginDto.UserName),
+            new Claim("creation_date_utc", DateTime.UtcNow.ToString())
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        await base.HttpContext.SignInAsync(
+            scheme: CookieAuthenticationDefaults.AuthenticationScheme,
+            principal: new ClaimsPrincipal(claimsIdentity)
+        );
+
+        if(string.IsNullOrWhiteSpace(loginDto.ReturnUrl)) 
+            return base.RedirectToAction(controllerName: "Books", actionName: "Get");
+
+        return base.RedirectPermanent(loginDto.ReturnUrl);
     }
 }
--- Dtos/LoginDto.cs
+++ Dtos/LoginDto.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SMlibraryApp.Dtos
+namespace SMlibraryApp.Dtos;
+public class LoginDto
 {
-    public class LoginDto
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
+    public string ReturnUrl {  get;  set;  }  
+    public string UserName { get; set; }
+    public string Password { get; set; }
 }
